c60b1e1ada228fea0b63ebe808f58a7fa894fbe4
Fukugami Makoto <lionel.nguyen1812@gmail.com>
2013-12-18 01:21:42 +0700

    first commit

---
+++ PowerSurvey/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PowerSurvey.Global" Language="C#" %>
---
+++ PowerSurvey/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PowerSurvey.Startup))]
+namespace PowerSurvey
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
