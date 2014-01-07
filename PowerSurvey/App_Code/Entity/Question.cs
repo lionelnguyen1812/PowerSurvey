fa897a80904e5b04e2df54728fd21af5b4148365
tinblanc <tin.zz123@gmail.com>
2014-01-08 01:01:47 +0700

    question

---
+++ PowerSurvey/App_Code/Entity/Question.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Question
+/// </summary>
+public class Question
+{
+    public int QuestionCreateBy { get; set; }
+
+    public string QuestionContent { get; set; }
+
+	public Question()
+	{
+		
+	}
+
+    public Question(int QuestionCreateBy,string QuestionContent) 
+    {
+        this.QuestionCreateBy = QuestionCreateBy;
+        this.QuestionContent = QuestionContent;
+    }
+
+}
